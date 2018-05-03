9eca33522cd95fb2fbf2e2e9c143dd0d874fc1b8
tutorial88 <38849092+tutorial88@users.noreply.github.com>
2018-05-03 14:38:48 +0700

    Add files via upload

---
+++ MYSQLVBNETClass/Connection.vb
@@ -0,0 +1,20 @@
+﻿Imports MySql.Data.MySqlClient
+Module Connection
+    Public Conn As New MySqlConnection
+    Dim Result As Boolean
+    Dim StrConn As String
+    Public Function OpenDB() As Boolean
+        Try
+            If Conn.State = ConnectionState.Closed Then
+                StrConn = "server=localhost;user=root;password=;port=3306;database=tutorial88_vbnet;sslmode=none"
+                Conn.ConnectionString = StrConn
+                Conn.Open()
+                Result = True
+            End If
+        Catch ex As Exception
+            Result = False
+        End Try
+
+        Return Result
+    End Function
+End Module
---
+++ MYSQLVBNETClass/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataToText()
+    End Sub
+
+    Sub DataToText()
+        If OpenDB() Then
+            Dim Query As String = "Select * from country limit 1"
+            Dim CMD As New MySqlCommand(Query, Conn)
+            Dim DTReader As MySqlDataReader
+
+            Try
+                DTReader = CMD.ExecuteReader
+
+                While DTReader.Read
+                    TextBox1.Text = DTReader.GetString("cnt_name")
+                End While
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                Conn.Close()
+            End Try
+
+        End If
+    End Sub
+End Class
